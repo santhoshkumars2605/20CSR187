d9fd739c805a257ce0c53264aa7d4a9e4de8fd1d
santhoshkumar187 <santhoshkumars.20cse@kongu.edu>
2023-07-26 12:09:07 +0530

    2.0

--- Task2/train/src/resources/components/Home.js
+++ Task2/train/src/resources/components/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import Card from 'react-bootstrap/Card';
 export default function Home() {
 const [tok,setTok]=useState();
-
+const [data,setData] = useState();
     useEffect(async()=>{
        await axios.post("http://20.244.56.144/train/auth",{
         companyName: "SkTrains",
@@ -12,23 +13,44 @@ const [tok,setTok]=useState();
         ownerEmail: "santhoshkumars.20cse@kongu.edu",
         rollNo: "20CSR187"
 
-    }).then((res)=>{
+    }).then(async(res)=>{
         
         setTok(res.data.access_token);
-    }).catch((err)=>{
-        console.log(err);
-    })
-    await axios.get("http://20.244.56.144/train/trains",{
+        await axios.get("http://20.244.56.144/train/trains",{
         headers: {
-          'Authorization': `Bearer ${tok}`
+          'Authorization': `Bearer ${res.data.access_token}`
         }
       }).then((res)=>{
-        console.log(res);
+
+        console.log(res.data);
+        setData(res.data);
     }).catch((err)=>{
         console.log(err);
     })
+    }).catch((err)=>{
+        console.log(err);
+    })
+    
     },[])
   return (
-    <div>Home</div>
+    <>
+    <Container>
+    {
+     data.map((items,index)=>{
+     return (<Card key={index} style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{items.trainName}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{items.trainNumber}</Card.Subtitle>
+        <Card.Text>
+            Seats Sleeper : {items.seatsAvailable.sleeper} Price : {items.price.sleeper}<br/>
+            Seats AC : {items.seatsAvailable.AC} Price : {items.price.AC} <br/>
+        </Card.Text>
+        <Card.Link href={`/train?id=${items.trainNumber}`}>Check</Card.Link>
+      </Card.Body>
+    </Card>)
+    })
+}
+    </Container>
+    </>
   )
 }
